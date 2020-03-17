7cd741a4406eca88588233364739f774655d5808
Mitchel Sellers <msellers@Iowacomputergurus.com>
2020-03-16 22:53:58 -0500

    SampleJob Test Implemented

---
+++ Source/TwilioDemo.Tests/SampleJobTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using Xunit;
+
+namespace TwilioDemo.Tests
+{
+    public class SampleJobTests
+    {
+        private readonly ISampleJob _sampleJob;
+        private readonly Mock<ISmsService> _smsServiceMock;
+
+        public SampleJobTests()
+        {
+            _smsServiceMock = new Mock<ISmsService>();
+            _sampleJob = new SampleJob(_smsServiceMock.Object);
+        }
+
+        [Fact]
+        public void Send_ShouldPassProperValuesToSmsService()
+        {
+            //Arrange
+            var toAddress = "+11111111111";
+            var message = "Testing";
+            
+            //Act
+            _sampleJob.SendMessage(toAddress, message);
+
+            //Assert
+            _smsServiceMock.Verify(s => s.Send(toAddress, message), Times.Once);
+        }
+    }
+}
